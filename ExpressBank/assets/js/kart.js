e13153a214697c4e41ec341cdb4fdf58eb5cb3b6
Miraghaa <miraghaea@code.edu.az>
2023-07-24 21:13:38 +0400

    Final

--- ExpressBank/assets/js/kart.js
+++ ExpressBank/assets/js/kart.js
@@ -55,136 +55,52 @@ if(localStorage.getItem('karts') === null){
     localStorage.setItem('karts',JSON.stringify([]))
 }
 
-    let btns = document.querySelector('.kart-info1 .card-asd a')
-
-    btns.onclick = () => {
-        let img = document.querySelector('.bsk .a2').src
-        let text = document.querySelector('.bsk h4').innerHTML
-        let text1 = document.querySelector('.bsk p').innerHTML
+    let btns = document.querySelectorAll('.ca .a')
+     
+    for(let md1 of btns){
+      md1.onclick = () => {
+        let mds = md1.parentElement.parentElement.parentElement.previousElementSibling.children[1].src
+        let mdt = md1.parentElement.parentElement.parentElement.children[0].innerHTML
+        let mdt2 = md1.parentElement.parentElement.parentElement.children[1].innerHTML
         let baskets = JSON.parse(localStorage.getItem('karts'))
-        let exist = baskets.find(x=> x.sekil === img)
-      
+        let exist = baskets.find(x=> x.sekil === mds)
+
         if(exist === undefined){
           baskets.push({
-              sekil: img,
-              yazi: text,
-              yazi1: text1
+              sekil: mds,
+              yazi: mdt,
+              yazi1: mdt2 
           })
         }
-        
         localStorage.setItem('karts',JSON.stringify(baskets))
-        show()   
+        show() 
+      }
     }
 
-    let btns2 = document.querySelector('.card-asg a')
+     let cc = document.querySelectorAll('.cc .a')
+     for(let md of cc ){
+      md.onclick=()=>{
+        let mds = md.parentElement.parentElement.parentElement.previousElementSibling.children[0].src
+        let mdt = md.parentElement.parentElement.parentElement.children[0].innerHTML
+        let mdt2 = md.parentElement.parentElement.parentElement.children[1].innerHTML
+        let baskets = JSON.parse(localStorage.getItem('karts'))
+        let exist = baskets.find(x=> x.sekil === mds)
 
-     btns2.onclick = () => {
-         let img = document.querySelector('.alltxt-photo4 img').src
-         let text = document.querySelector('.alltxt-text h4').innerHTML
-         let text1 = document.querySelector('.alltxt-text p').innerHTML
-         let baskets = JSON.parse(localStorage.getItem('karts'))
-         let exist = baskets.find(x=> x.sekil === img)
-      
-         if(exist === undefined){
-           baskets.push({
-               sekil: img,
-               yazi: text,
-               yazi1: text1
-           })
-         }
-         
-         localStorage.setItem('karts',JSON.stringify(baskets))
-         show()
-    }
-
-    let btns3 = document.querySelector('.card-ash a')
-
-    btns3.onclick = () => {
-          let img = document.querySelector('.bs img').src
-          let text = document.querySelector('.bs h4').innerHTML
-          let text1 = document.querySelector('.bs p').innerHTML
-          let baskets = JSON.parse(localStorage.getItem('karts'))
-          let exist = baskets.find(x=> x.sekil === img)
-      
-          if(exist === undefined){
-            baskets.push({
-                sekil: img,
-                yazi: text,
-                yazi1: text1
-            })
-          }
-          localStorage.setItem('karts',JSON.stringify(baskets))
-          show()    
-    }
+        if(exist === undefined){
+          baskets.push({
+              sekil: mds,
+              yazi: mdt,
+              yazi1: mdt2
+          })
+        }
+        localStorage.setItem('karts',JSON.stringify(baskets))
+        show() 
+      }
+     }
+    
 
     function show() {
         let basketa  = JSON.parse(localStorage.getItem('karts'))
         document.getElementById('count').innerHTML = basketa.length
     }
-    
-let btnss = document.querySelector('.kart-info1 .card-asda a')
-    
-  btnss.onclick = () => {
-    let img = document.querySelector('.bska .a3').src
-    let text = document.querySelector('.bska h4').innerHTML
-    let text1 = document.querySelector('.bska p').innerHTML
-    let baskets = JSON.parse(localStorage.getItem('karts'))
-    let exist = baskets.find(x=> x.sekil === img)
-  
-    if(exist === undefined){
-      baskets.push({
-          sekil: img,
-          yazi: text,
-          yazi1: text1
-      })
-    }
-    
-    localStorage.setItem('karts',JSON.stringify(baskets))
-    show()
-
- }
-
- let btnsss = document.querySelector('.kart-info1 .card-asdaa a')
-    
-  btnsss.onclick = () => {
-    let img = document.querySelector('.bskaa img').src
-    let text = document.querySelector('.bskaa h4').innerHTML
-    let text1 = document.querySelector('.bskaa p').innerHTML
-    let baskets = JSON.parse(localStorage.getItem('karts'))
-    let exist = baskets.find(x=> x.sekil === img)
-  
-    if(exist === undefined){
-      baskets.push({
-          sekil: img,
-          yazi: text,
-          yazi1: text1
-      })
-    }
-    
-    localStorage.setItem('karts',JSON.stringify(baskets))
-    show()
-
- }
-
- let btnson = document.querySelector('.kart-info1 .card-asdson a')
-    
-  btnson.onclick = () => {
-    let img = document.querySelector('.bskson img').src
-    let text = document.querySelector('.bskson h4').innerHTML
-    let text1 = document.querySelector('.bskson p').innerHTML
-    let baskets = JSON.parse(localStorage.getItem('karts'))
-    let exist = baskets.find(x=> x.sekil === img)
-  
-    if(exist === undefined){
-      baskets.push({
-          sekil: img,
-          yazi: text,
-          yazi1: text1
-      })
-    }
-    
-    localStorage.setItem('karts',JSON.stringify(baskets))
-    show()
-
- }
     show()
